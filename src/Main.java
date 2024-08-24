f3fa328c36117f3f73d739d12a2e076c1a1191c8
Dhruvsinghdk <re621006@gmail.com>
2024-08-24 21:35:40 +0530

    new

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 0b110;
+        System.out.println(a);
+    }
+}
--- src/Mymain.java
+++
@@ -1,5 +0,0 @@
-public class Mymain {
-    public static void main(String[] args) {
-        System.out.println("hello");
-    }
-}
